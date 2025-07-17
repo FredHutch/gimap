acb10de6dec2d56ff71d7072767263411f59f43a
Candace Savonen <cansav09@gmail.com>
2025-07-17 13:01:19 -0400

    Make skip figshare work

--- tests/testthat/test-figshare.R
+++ tests/testthat/test-figshare.R
@@ -2,26 +2,26 @@ skip_if_figshare_unavailable <- function() {
   tryCatch({
     httr::HEAD("https://figshare.com", httr::timeout(5))
   }, error = function(e) {
-    testthat::skip("Figshare unavailable")
+    "Figshare unavailable"
   })
 }
-
-test_that("Figshare API download works", {
-  testthat::skip_on_cran()
-  skip_if_figshare_unavailable()
+if (skip_if_figshare_unavailable() != "Figshare unavailable") {
+  test_that("Figshare API download works", {
+    testthat::skip_on_cran()
+    
+    data_list <- get_figshare(return_list = TRUE)
   
-  data_list <- get_figshare(return_list = TRUE)
-
-  testthat::expect_type(data_list, "list")
-
-  get_figshare(file_name = "Achilles_common_essentials.csv")
-
-  testthat::expect_true(
-    file.exists(
-      file.path(
-        system.file("extdata", package = "gimap"),
-        "Achilles_common_essentials.csv"
+    testthat::expect_type(data_list, "list")
+  
+    get_figshare(file_name = "Achilles_common_essentials.csv")
+  
+    testthat::expect_true(
+      file.exists(
+        file.path(
+          system.file("extdata", package = "gimap"),
+          "Achilles_common_essentials.csv"
+        )
       )
     )
-  )
-})
+  })
+}
