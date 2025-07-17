ac65c19b2434a1cbbb1d6083f9b942783538c74c
Candace Savonen <cansav09@gmail.com>
2025-07-17 14:00:32 -0400

    Skip tests when we don't have figshare access

--- tests/testthat/test-figshare.R
+++ tests/testthat/test-figshare.R
@@ -1,20 +1,23 @@
 skip_if_figshare_unavailable <- function() {
-  tryCatch({
-    httr::HEAD("https://figshare.com", httr::timeout(5))
-  }, error = function(e) {
-    "Figshare unavailable"
-  })
+  tryCatch(
+    {
+      httr::HEAD("https://figshare.com", httr::timeout(5))
+    },
+    error = function(e) {
+      "Figshare unavailable"
+    }
+  )
 }
 if (skip_if_figshare_unavailable()[1] != "Figshare unavailable") {
   test_that("Figshare API download works", {
     testthat::skip_on_cran()
-    
+
     data_list <- get_figshare(return_list = TRUE)
-  
+
     testthat::expect_type(data_list, "list")
-  
+
     get_figshare(file_name = "Achilles_common_essentials.csv")
-  
+
     testthat::expect_true(
       file.exists(
         file.path(
--- tests/testthat/test-run_qc.R
+++ tests/testthat/test-run_qc.R
@@ -1,27 +1,39 @@
-test_that("HTML file is created and content is correct", {
-  testthat::skip_on_cran()
-
-  # Does the thing run?
-  gimap_dataset <- get_example_data("gimap")
-
-  html_file <- run_qc(gimap_dataset,
-    output_file = tempfile(),
-    plots_dir = tempdir(),
-    open_results = FALSE,
-    overwrite = TRUE
+skip_if_figshare_unavailable <- function() {
+  tryCatch(
+    {
+      httr::HEAD("https://figshare.com", httr::timeout(5))
+    },
+    error = function(e) {
+      "Figshare unavailable"
+    }
   )
+}
+if (skip_if_figshare_unavailable()[1] != "Figshare unavailable") {
+  test_that("HTML file is created and content is correct", {
+    testthat::skip_on_cran()
 
-  expect_true(file.exists(html_file))
+    # Does the thing run?
+    gimap_dataset <- get_example_data("gimap")
 
-  # Test the other dataset too
-  gimap_dataset <- get_example_data("gimap_treatment")
+    html_file <- run_qc(gimap_dataset,
+      output_file = tempfile(),
+      plots_dir = tempdir(),
+      open_results = FALSE,
+      overwrite = TRUE
+    )
 
-  html_file <- run_qc(gimap_dataset,
-    output_file = tempfile(),
-    plots_dir = tempdir(),
-    open_results = FALSE,
-    overwrite = TRUE
-  )
+    expect_true(file.exists(html_file))
 
-  expect_true(file.exists(html_file))
-})
+    # Test the other dataset too
+    gimap_dataset <- get_example_data("gimap_treatment")
+
+    html_file <- run_qc(gimap_dataset,
+      output_file = tempfile(),
+      plots_dir = tempdir(),
+      open_results = FALSE,
+      overwrite = TRUE
+    )
+
+    expect_true(file.exists(html_file))
+  })
+}
