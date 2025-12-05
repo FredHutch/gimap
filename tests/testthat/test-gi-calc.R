d816d45b92fbc6b8932633eeb7cf5ff71afa4ec6
Candace Savonen <cansav09@gmail.com>
2025-12-05 10:26:38 -0500

    Fix bug

--- tests/testthat/test-gi-calc.R
+++ tests/testthat/test-gi-calc.R
@@ -162,13 +162,13 @@ if (skip_if_figshare_unavailable()) {
       calc_gi(use_ntc = FALSE)
 
     plot_exp_v_obs_scatter(gimap_dataset_no_ntc)
-    plot_exp_v_obs_scatter(gimap_dataset)
+    plot_exp_v_obs_scatter(gimap_dataset_no_ctrl)
 
     plot_rank_scatter(gimap_dataset_no_ntc)
-    plot_rank_scatter(gimap_dataset)
+    plot_rank_scatter(gimap_dataset_no_ctrl)
 
     plot_volcano(gimap_dataset_no_ntc)
-    plot_volcano(gimap_dataset)
+    plot_volcano(gimap_dataset_no_ctrl)
 
     # Check that gi_score values are not all NA
     testthat::expect_false(all(is.na(gimap_dataset_no_ctrl$gi_scores$gi_score)))
